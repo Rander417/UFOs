bab82cac54e0d1f312f962e6c8a131675553e421
Richard Anderson <rich417@gmail.com>
2020-08-02 19:53:50 -0400

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# UFOs
+vx
+
+
+## Summary
+The purpose of this project was to create a website where Dana's recent articale on UFO's could be displayed along with some supporting information. THis presents a very accessibile way to read her material and also sort through the large dataset of sightings.
+
+
+## Technoligies used
+Primarily HTML/CSS/Javascript.
+- HTML was used to create website framing and setup various webpage elements such as the search bxoes. CSS & Bootstrap used to add additional styling
+- Javascript to import and filter the data for display by the html
+
+## Further development
+- The ability for users to submit requests to log a new sighting
+- Updating the news section to automocally scroll through different excerts
+- The addition of a recommeded videos frame
+- Links to the author's contact information and profile page
+- Refactor the updateFilters() function to use a loop and be able to accomodate a dynamic number of total filters
+
+![]![](readme_images/fullscreen.png)
--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,13 @@ const tableData = data;
 // D3 is a JavaScript library that produces sophisticated and highly dynamic graphics in an HTML webpage
 var tbody = d3.select("tbody");
 
+// Keep track of all filters
+//var filters = {datetime: "1/13/2010", city: "ackerman"};
+var filters = {datetime: "", city: "", state: "", country: "", shape: ""};
+
+///////////////////////////////////////////////////////////////////////////////
+
+
 // Build the table to display all of the UFO sightings
 function buildTable(data) {
     // First, clear out any existing data
@@ -25,16 +32,12 @@ function buildTable(data) {
       );
     });
 }
+///////////////////////////////////////////////////////////////////////////////
 
-// Keep track of all filters
-//var filters = {datetime: "1/13/2010", city: "ackerman"};
-var filters = {datetime: "", city: "", state: "", country: "", shape: ""};
-
-//CHALLENGE FUNCTION
-// This function will replace your handleClick function
+//Read input from user and update the list of filters
 function updateFilters() {
 
-  // Save the value, and id of all filters
+  // Save the values, and ids of all filters
   let date_filter_value = d3.select("#datetime").property("value");
   let date_filter_id = d3.select("#datetime").property("id");
 
@@ -49,7 +52,6 @@ function updateFilters() {
 
   let shape_filter_value = d3.select("#shape").property("value");
   let shape_filter_id = d3.select("#shape").property("id");
-
 
   //If-else blocks to remove unused filters
   if (date_filter_value != ""){
@@ -82,54 +84,34 @@ function updateFilters() {
     delete filters.shape;
   }
 
-  console.log(Object.keys(filters).length)
-  console.log(Object.values(filters).length)
-  console.log(filters)
-
   // Call function to apply all filters and rebuild the table
   filterTable();
 }
 ///////////////////////////////////////////////////////////////////////////////
 
-//*&^&^%%$#$@@$ DELETE THIS BEFORE SUBMISSION
-function handleClick() {
-  // Grab the datetime value from the filter
-  let date = d3.select("#datetime").property("value");
-  let filteredData = tableData;
-  
-  // Check to see if a date was entered and filter the
-  // data using that date.
-  if (date) {
-    // Apply `filter` to the table data to only keep the
-    // rows where the `datetime` value matches the filter value
-    filteredData = filteredData.filter(row => row.datetime === date);
-  }
-  
-  // Rebuild the table using the filtered data
-  // @NOTE: If no date was entered, then filteredData will just be the original tableData.
-  buildTable(filteredData);
-}
-///////////////////////////////////////////////////////////////////////////////
-  
+
 function filterTable() {
 
   // Set the filteredData to the tableData
   let filteredData = tableData;
+  let filterCounter = 0;
 
   // Loop through all of the filters and keep any data that
-  // matches the filter values
   filter_Keys = Object.keys(filters)
 
-  for (x in filter_Keys){
-    filterName = Object.keys(filters)[x]
-    filteredData = filteredData.filter(row => row[filterName] === filters[filterName]);
-  }
+  filter_Keys.forEach(applyFilters);
 
+  function applyFilters(){
+    filterName = Object.keys(filters)[filterCounter]
+    filteredData = filteredData.filter(row => row[filterName] === filters[filterName]);
+    filterCounter++
+  }
+  
 
   // Finally, rebuild the table using the filtered Data
   buildTable(filteredData);
 }
-
+///////////////////////////////////////////////////////////////////////////////
 
 // Attach an event to listen for the form button
 // Hint: You'll need to select the event and what it is listening for within each set of parenthesis
